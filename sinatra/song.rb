faf131b2c1d1d31ed797fcf476a63713dd3b9171
Gitica Makhnotra <geetika9@hotmail.com>
2016-01-30 15:18:57 -0800

    third commit

--- sinatra/song.rb
+++ sinatra/song.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
+enable :sessions
 
 get '/' do
-  "Hello World"
-erb :index
+
+erb :index, layout: :default_layout
+end
+
+get '/new' do
+
+  erb :add_song ,layout: :default_layout
+end
+
+post '/'do
+  "artist: #{params[:artist]}, title: #{params[:title]}, video: #{params[:video]}"
+  erb :index, layout: :default_layout
 end
